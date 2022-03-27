050e984a1a76ac9cb80c9fd191b07eb5eaa4369b
MIGUEL OLIVEIRA <migueloliveiradasneves@gmail.com>
2022-03-27 16:12:47 -0300

    atualizações

---
+++ fazer degrade simples.cs
@@ -0,0 +1,10 @@
+﻿protected override void OnPaintBackground(PaintEventArgs e)
+{
+    using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle,
+                                                               Color.FromArgb(36, 41, 47),
+                                                               Color.FromArgb(36, 40, 51),
+                                                               50F))
+    {
+        e.Graphics.FillRectangle(brush, this.ClientRectangle);
+    }
+}
